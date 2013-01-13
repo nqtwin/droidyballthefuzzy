eb40f3f2be78155d34b730a749b70fa6dff8cf43
Nicole <nquah@thoughtworks.com>
2013-01-13 02:01:37 -0500

    Added extra fields to db Entry model

--- src/main.py
+++ src/main.py
@@ -108,8 +108,11 @@ class Authorization():
 class Entry(db.Model):
 	title = db.StringProperty(required=True)
 	description = db.StringProperty(required=True,multiline=True)
-	#location = db.StringProperty(required=True)
+	location = db.StringProperty(required=True,default="Boston")
+	author = db.StringProperty(required=True,default="Arthur")
 	created_on = db.DateTimeProperty(auto_now_add=True)
+	accomplished_on = db.DateTimeProperty
+	is_accomplished = db.BooleanProperty(default=False)
 	id = db.StringProperty(required=True)
 
 # URI mapping for app engine
