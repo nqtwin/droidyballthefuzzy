f06a9ef64b393c49818c8aece6ed6557ab4ee153
Nicole <nquah@thoughtworks.com>
2013-01-25 02:20:56 -0800

    started adding javascript for  marking an entry done

--- scripts/single_entry.js
+++ scripts/single_entry.js
@@ -3,7 +3,7 @@ $(className).mouseover(function() { $(this).css("color",color) });
 }
 
 $(document).ready(function() {
-$(".entry-title").mouseleave(function() { $(this).css("color","blue") });
-$("#checkbox").css("visibility","visible");
-$(".entry-title").css("color","blue");
+//$(".entry-title").mouseleave(function() { $(this).css("color","blue") });
+//$("#checkbox").css("visibility","visible");
+//$(".entry-title").css("color","blue");
 });
