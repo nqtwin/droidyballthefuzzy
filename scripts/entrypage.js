26de2dc341bbaddd95d73e541c290b2a5e38dcb7
asafira <arthur.safira@gmail.com>
2013-02-08 04:17:15 -0500

    Added javascript transitions to landing page

--- scripts/entrypage.js
+++ scripts/entrypage.js
@@ -1,7 +1,25 @@
-var str = $('Hello World').text();
+	var str = "Live your lives with the desire to discover what's out there.";
 
-var spans = '<span>' + str.split(/\s+/).join(' </span><span>') + '</span>';
-
-$(spans).hide().appendTo('#letter-content').each(function(i) {
-    $(this).delay(400 * i).fadeIn();
+	var spans = '<span>' + str.split("").join('</span><span>') + '</span>';
+	
+	var strWithLink = "Live your lives with the desire to discover <a href= {{url}}> what's out there.  </a>";
+	
+	var DELAY_TIME = 60;
+	
+	var FADE_IN_TIME = 1000;
+	
+	
+$(document).ready(function() {
+	$(spans).hide().appendTo('#letter-content').each(function(i) {
+    	$(this).delay(DELAY_TIME * i).fadeIn(FADE_IN_TIME);
+	});
+	
+	var waitTime = 60 * str.split("").length + FADE_IN_TIME;
+	
+	setTimeout(
+  		function() 
+  		{
+    		$('#letter-content').html(strWithLink);
+  		}, waitTime);
+	
 });
